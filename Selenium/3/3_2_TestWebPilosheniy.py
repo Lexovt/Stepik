44ff44ee0b740a7ea088da31e07649ff00aeb762
kadevops90 <kadevops90@gmail.com>
2024-10-04 23:26:24 +0300

    New Lessons Python

---
+++ Selenium/3/3_2_TestWebPilosheniy.py
@@ -0,0 +1,32 @@
+# assert abs(-42) == 42
+
+#assert abs(-42) == -42
+
+#assert abs(-42) == -42, "Should be absolute value of a number"
+
+'''
+actual_result = "abrakadabra"
+print("Wrong text, got " + actual_result + ", something wrong")
+'''
+
+#print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+
+'''
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+'''
+
+#ZADANIE
+#1
+'''
+def test_input_text(expected_result, actual_result):
+   assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+'''
+#2
+'''
+def test_substring(full_string, substring):
+   assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+'''
+
